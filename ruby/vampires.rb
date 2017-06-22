2d068d838b14b52c9a93073c9f4879dc80b88c92
Mitch Mele <mitchmele@gmail.com>
2017-06-22 07:55:53 -0500

    Add basic questions to ask new employee.

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+def vamp_finder
+  puts "What is your name?"
+  name = gets.chomp
+
+  puts "How old are you? What year were you born?"
+  age = gets.chomp
+
+  puts "Would you like some garlic bread?"
+  garlic = gets.chomp
+
+  puts "Would you like health insurance? (Y/N)"
+  insurance = gets.chomp
+
+end

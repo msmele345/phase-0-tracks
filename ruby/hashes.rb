fb34d03d885c017e1aae753a74da4abddbd44ce3
Mitch Mele <mitchmele@gmail.com>
2017-06-28 17:45:35 -0500

    Update driver code logic.

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -4,16 +4,16 @@
 #Create driver code below to ask the designer questions
 #
 
-app = {
-  :client_name => name
-  :client_age => age
-  :number_of_children => kids
-  :decor_theme=> theme
-  :previous_design => past
-}
+# app = {
+#   :client_name => name,
+#   :client_age => age,
+#   :number_of_children => kids,
+#   :decor_theme=> theme,
+#   :previous_design => past
+# }
 
 
-puts app[:name]
+# puts app[:name]
 
 
 #DRIVER CODE
@@ -32,13 +32,33 @@ theme = gets.chomp
 puts "A designer has been hired in the past (True/False)"
 past = gets.chomp
 
-puts "Do you need to change any information on the form?"
+puts "Do you need to change any information on the form? If not, please enter None"
 changes = gets.chomp
 
 
+app = {
+  :client_name => name,
+  :client_age => age,
+  :number_of_children => kids,
+  :decor_theme=> theme,
+  :previous_design => past
+}
 
+#Check to see if user wants to change any information before submitting. If the answer is not NONE, then find a way to take the user input and modify the hash.
+#User conditional logic to do this below
+#How can I take user input on modified answer and replace existing hash rather than add a new key value pair?
 
-
+if changes != "None"
+  puts "Which section would you like to change?"
+    modification = gets.chomp.to_sym
+  puts "What is the correct information?"
+    changed_answer = gets.chomp
+    # app[:modification] = changed_answer
+    app[:"#{modification}"] = changed_answer
+    p app
+ else
+  p app
+end
 
 
 
@@ -48,5 +68,3 @@ changes = gets.chomp
 # if :hired == true
 #  app[:success] ="yes"
 # end
-
-p app

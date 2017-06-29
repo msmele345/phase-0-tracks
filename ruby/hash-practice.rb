e8069f0ed343da15932ae8b2e2bbe4d83a2a9681
Mitch Mele <mitchmele@gmail.com>
2017-06-29 13:19:12 -0500

    Add practice ruby file.

--- ruby/hash-practice.rb
+++ ruby/hash-practice.rb
@@ -1,6 +1,4 @@
-#Create hash for application questions
-#Have each question be a key, each answer be the value
-#print hash and manipulate keys for testing
+
 
 app = {
   :name => "Mitch",
@@ -15,18 +13,9 @@ puts app[:name]
 
 
 
-
-
-
-
-
-
-
-#DRIVER CODE
-puts "Name test"
-
-if :hired == true
- app[:success] ="yes"
-end
-
-p app
+#Explore the thing - Array practice from earlier in the challenge:
+Array = [12,2,2,3,6,10]
+Array[2] => 3
+Array.slice[-1] => 10
+Array.uniq! =>[12,2,3,6,10]
+Array

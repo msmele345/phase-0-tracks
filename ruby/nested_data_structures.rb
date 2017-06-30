dc2ae50eadcb45f3974c041fb9d2f4de354635d8
Mitch Mele <mitchmele@gmail.com>
2017-06-29 20:01:10 -0500

    Setup nested classroom data structure.

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,14 @@
+classroom = {
+  class_grade: '5th Grade',
+  desk_types : {
+    standing_desk: 8,
+    sitting_desks: 5
+  },
+  supplies: ["pencils","paper", "tape", "markers", "ruler", "scissors"],
+  teachers: {
+    "Mr. Smartypants",
+    "Mrs. Supercool",
+    "Mr. Badnews"
+  }
+}
+

cc0fff1223fae8e4054153a6444fba0b7f7f3979
Mitch Mele <mitchmele@gmail.com>
2017-06-29 21:36:13 -0500

    Build classroom hash with nested array for supplies.

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,14 +1,25 @@
 classroom = {
-  class_grade: '5th Grade',
-  desk_types : {
-    standing_desk: 8,
-    sitting_desks: 5
-  },
-  supplies: ["pencils","paper", "tape", "markers", "ruler", "scissors"],
-  teachers: {
-    "Mr. Smartypants",
-    "Mrs. Supercool",
-    "Mr. Badnews"
+  fith_grade:{
+    desk_types:{
+      standing_desks: 8,
+      sitting_desks:4
+    },
+    supplies: ["pencils","paper", "tape", "markers", "ruler", "scissors"],
+    teachers: {
+      classroom1: 'Mr. Knowitall',
+      classroom2: 'Mrs. Supercool',
+      classroom3: 'Mr. Badnews'
+    },
   }
 }
 
+
+
+
+puts classroom[:fith_grade][:teachers][:classroom1]
+
+puts classroom[:fith_grade][:desk_types][:standing_desks]
+
+puts classroom[:fith_grade][:supplies][3]
+
+puts classroom[:fith_grade][:teachers][:classroom2].upcase

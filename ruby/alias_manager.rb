984eb1b32bda1087c73619650186d6604578501e
Mitch Mele <mitchmele@gmail.com>
2017-07-03 12:26:34 -0500

    Add additional methods to call in conditional logic.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -4,29 +4,43 @@
 #Reverse the two words using .reverse and join them back together.
 #Use downcase to lowercse all letters
 #Create vowels and non vowel variables to use as reference points
-#loop through string and determine if current letter is a vowel or non vowel. Push next vowel or non vowel into empty string
+#loop through string and determine if current letter is a vowel or non vowel.
+#Create seperate methods to advance letters from current vowel or non vowel to next
+#call methods once a vowel or non vowel is found while iterating
 #Account for spaces in the conditional logic
-#Capitalize final first leters of each words in final string before printing
+#Capitalize first letter of each new word in final string before printing
+
+def vowel(letter)
+  vowels = "aeiou"
+  vowel_index = vowels.index(letter) + 1
+  next_letter = vowels[vowel_index]
+end
+
+def non_vowel(letter)
+non_vowels = "bcdfghjklmnpqrstuvwxyz"
+non_vowel_index = non_vowels.index(letter) + 1
+next_letter = non_vowels[non_vowel_index]
+end
+
 
 def letter_changer(string)
 vowels = "aeiou"
 non_vowels = "bcdfghjklmnpqrstuvwxyz"
 string = string.split.reverse.join(' ').downcase
-#p string
 final_string = ''
 letter = 0
 while letter < string.length
   if vowels.include?(string[letter])
-    final_string+=string[letter].next
+    final_string += vowel(string[letter])
   elsif non_vowels.include?(string[letter])
-    final_string+=string[letter].next
+    final_string += non_vowel(string[letter])
   elsif string[letter] == " "
-    final_string+=" "
+    final_string += " "
   end
   letter+=1
 
 end
-#capitalize each first letter in the string
+#capitalize each first letter in the string using map
 p final_string.split.map(&:capitalize).join(' ')
 end
 

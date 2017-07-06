1dbc3736874a8a8ac759ca705e10513e0d59e60f
Mitch Mele <mitchmele@gmail.com>
2017-07-06 17:51:03 -0500

    Add final methods .each to print results.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -41,10 +41,58 @@ while letter < string.length
 
 end
 #capitalize each first letter in the string using map
+
 p final_string.split.map(&:capitalize).join(' ')
 end
 
 #Driver Code
 
-letter_changer("Felicia Torres")
-letter_changer("Mitch Mele")
+#Create empty hash to be used for the data structure
+#Setup basic loop to repeatedly ask user for names that need to be changed to secret alias
+#Call the letter changer method and pass in the user input to generatge secret alias. Store this name in a variable fake name
+#Use .store to place the fake name and original name (user input) as key value pairs
+#Break out of loop when user hits enter on the keyboard (blank entry)
+#Iterate through hash and print the key value pairs with "is actually" in between them
+
+
+master_file = {}
+
+puts "Secret name generator. WARNING! This machine creates secret undercover names and should only be used for classified operations."
+
+puts "To get started, please enter a real name. The machine will generate a secret name and store it in a file that you can access later. When you are finished, please press enter on your keyboard."
+
+loop do
+
+  puts "What name do you need changed?"
+  user_input = gets.chomp
+
+  puts "Sweet, here is a secret name. It will be stored in the secret name database."
+  fake_name = letter_changer(user_input)
+  master_file.store(user_input,fake_name)
+
+  #puts master_file
+
+  if user_input.empty?
+    break
+  end
+end
+
+#Setup next statement to pass over the blank entry that was generated from hitting enter to break out of the loop
+
+master_file.each do |i,v|
+  next if i == ""
+  p i + " is actually " + v
+end
+
+# Delete unwanted "" key (generated from hitting enter to break out of the loop) from the hash
+
+master_file.delete("")
+
+p master_file
+
+
+
+#Test cases for letter changer method
+
+# letter_changer("Felicia Torres")
+# letter_changer("Mitch Mele")

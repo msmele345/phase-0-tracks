109f112a8c557d29b93471dbfce314ff38e6c5aa
Mitch Mele <mitchmele@gmail.com>
2017-07-09 17:44:54 -0500

    Initial commit for release 0

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+# SPECIES ------------------------
+# Puppy
+
+# CHARACTERISTICS ----------------
+# Attention Starved
+# Friendly: yes
+# Eye count: 2
+# Name: varies
+# Color: varies
+
+# BEHAVIOR -----------------------
+# Jump
+# Run
+# Love
+# Bark

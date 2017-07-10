ad8e7f76b88cb85a30e4bc341d0d3a4d5bd22f44
Mitch Mele <mitchmele@gmail.com>
2017-07-10 15:22:55 -0500

    Finish release 2.

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,60 +1,111 @@
-# Business Logic
+# # Business Logic
 
-class Puppy
+# class Puppy
 
-  def fetch(toy)
-    puts "The puppy brought back the #{toy}"
-    toy
+#   def fetch(toy)
+#     puts "The puppy brought back the #{toy}"
+#     toy
+#   end
+
+#   def speak(number)
+#       number.times {print "Woof", " "}
+#   end
+
+#   def roll_over
+#     puts "Roll Over"
+#   end
+
+#   def dog_years(age)
+#     p age * 7
+#   end
+
+#   def shake_hands_w_paw(name)
+#    puts "Puppy, please shake hands with our friend #{name}"
+#   end
+
+#   def initialize    #Always spell check initialize. It will create a new print this method anytime a new instance is made
+#     puts "Initializing new puppy instance"
+#   end
+
+# end
+
+
+
+# # Driver Code
+
+# fido = Puppy.new
+# fido.fetch("ball")
+# puts "Oh yea!"
+
+# spot = Puppy.new
+# spot.speak(5)
+# puts "- Looking good!"
+
+
+# winnie = Puppy.new
+# winnie.roll_over
+# puts "Correcto Mundo!"
+
+# oscar = Puppy.new
+# oscar.dog_years(7)
+# puts "Math adds up!"
+
+# sparky = Puppy.new
+# sparky.shake_hands_w_paw("Allison")
+# puts "Shake hands instance works!"
+
+
+#Aglo for Release 2
+#Create a new class - anything
+#Create two methods to be used within the class (attribute)
+#Initialize method
+#Loop through instances of the class (puppy names)
+#Create data structure to store data in
+#Loop over data structure and call all intances for each iteration
+
+
+
+class Cars
+
+  def initialize
+    # @name = name
+    puts "Here is the car name"
   end
 
-  def speak(number)
-      number.times {print "Woof", " "}
+  def honk(number)
+    number.times { puts "Honk"}
   end
 
-  def roll_over
-    puts "Roll Over"
-  end
-
-  def dog_years(age)
-    p age * 7
-  end
-
-  def shake_hands_w_paw(name)
-   puts "Puppy, please shake hands with our friend #{name}"
-  end
-
-  def initialize    #Always spell check initialize. It will create a new print this method anytime a new instance is made
-    puts "Initializing new puppy instance"
+  def capacity(passengers)
+     "This car fits #{passengers}"
   end
 
 end
 
 
+# truck = Cars.new
+# truck.honk(8)
+# puts truck.capacity(4)
 
 
-# Test Code
+#Driver Code
 
 
 
-# Driver Code
+car_array = []
 
-a = Puppy.new
-a.fetch("ball")
-puts "Oh yea!"
+50.times do
+  car = Cars.new
+  car_array << car
+end
 
-b = Puppy.new
-b.speak(5)
-puts "- Looking good!"
+car_array.each do |x|
+  x.honk(rand(1..4))
+  puts x.capacity(rand(1..7))
+end
 
 
-c = Puppy.new
-c.roll_over
-puts "Correcto Mundo!"
 
-d = Puppy.new
-d.dog_years(7)
-puts "Math adds up!"
 
-e = Puppy.new
-e.shake_hands_w_paw("Allison")
-puts "Shake hands instance works!"
+
+

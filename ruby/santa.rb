8aa6efb5b1617148e6852629f1e068200fa0e299
Mitch Mele <mitchmele@gmail.com>
2017-07-10 20:18:42 -0500

    Add Santa class with initial instances.

---
+++ ruby/santa.rb
@@ -0,0 +1,33 @@
+# BUSINESS LOGIC ALGO
+
+
+class Santa
+
+  def initialize
+    puts "initializing Santa Instance"
+  end
+
+  def speak
+    puts "Ho, ho, ho! Haaaappy holidays!"
+  end
+
+  def eat_milk_and_cookies(cookie)
+    puts "That was a good #{cookie}!"
+  end
+
+end
+
+
+
+fake_santa = Santa.new
+fake_santa.speak
+fake_santa.eat_milk_and_cookies("Chocolate Chip")
+
+
+# TEST CODE
+
+
+
+
+
+# DRIVER CODE

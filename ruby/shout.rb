f02cefa6f2b2c4b8c43c99ddbf6c1e81562ba1e6
Mitch Mele <mitchmele@gmail.com>
2017-07-12 17:53:24 -0500

    Add methods to shout.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,21 @@
-module Shout
+#Require_relative to load new modules
+#Make sure to use ''
+#Modules can be mixins to classes - like a toolbelt for any once class instance
 
+module Shout
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def self.yell_happily(words)
+     words + "!!!" + " :)"
+  end
 end
+
+#DRIVER CODE
+
+
+Shout.singleton_methods
+Shout.class
+Shout.yell_angrily("This is tough")
+Shout.yell_happily("Ahh yea, I got it")

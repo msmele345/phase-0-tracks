e3bc3e3d4385658fcf509de57190bd052db39c97
Mitch Mele <mitchmele@gmail.com>
2017-07-13 22:41:40 -0500

    Final instance methods added for ToDolistclass. Rspec tests are all green.

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -1,5 +1,8 @@
 #Create a new class that will match the spec files class tests
-#Write instances methods that will match the methods being in the tests.
+#Write methods that will match the methods being in the tests.
+#Run tests in terminal - read errors and make changes as neccessary
+#Input = create code for instance class methods for TodoList class
+#Output =
 
 class TodoList
 
@@ -10,4 +13,20 @@ class TodoList
   def get_items
     @list
   end
+
+  def add_item(item)
+    @list.push(item)
+    @list
+  end
+
+  def delete_item(remove_item)
+    @list.delete(remove_item)
+    @list
+  end
+
+  def get_item(index)
+    @list[index]
+  end
+
+
 end

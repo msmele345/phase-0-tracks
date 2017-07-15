bd981152d08837a24e6c577aa3b365eb69490183
Mitch Mele <mitchmele@gmail.com>
2017-07-15 14:26:55 -0500

    Player 2 UI design added for secret word. Add about instance method to display current status after each guess.

--- ruby/week6solo/word_guess.rb
+++ ruby/week6solo/word_guess.rb
@@ -15,12 +15,17 @@
 #use module mixin for instance methods?
 
 class GuessGame
-  # attn_accessor :secret_word
-  attr_reader :current_count, :guess_check, :guess_tracker
+  attr_accessor :secret_word
+  attr_reader :guess_check, :guess_tracker, :about
   def initialize
     @guess_log = []
     @guess_count = 3
     @game_over = false
+  end
+
+  def secret_word (word)
+    @secret_word = word
+    @secret_word
   end
 
   def guess_check(guessed_word)
@@ -29,18 +34,20 @@ class GuessGame
       @guess_log << @guessed_word
       @guess_count = @guess_count - 1
     end
-     p @guess_log  # + "#{@guess_count}"?
-     p "Guesses remaining: #{@guess_count}"
+    # p @guess_log  # + "#{@guess_count}"?
+    # p "Guesses remaining: #{@guess_count}"
   end
 
-  def current_count
-    puts "Current attempts: #{@guess_count}"
+  def word_display
+    # @current_status = loop through secret word, if letter[i] is in secret word, print letter of i. If not, print "-" !!!!! STOPPING POINT
+     @secret_word
   end
 
-  def guess_tracker
-    @guess_log
+  def about
+    puts "Here is your current list of word guesses: #{@guess_log}"
+    puts "Guesses remaining: #{@guess_count}"
+    puts word_display
   end
-
 
 end
 
@@ -53,18 +60,31 @@ end
 
 
 
-puts "Time to play GUESS A YOUR FRIENDS SECRET WORD"
-game = GuessGame.new
+puts "Time to play GUESS YOUR FRIENDS SECRET WORD"
+  game = GuessGame.new
+puts()
+
+puts "Player A, please guess what the secret word is. Enter only one guess at a time."
+puts()
+
+puts "Player B, please pick a secret word so we can get started. Write it   down on a piece of paper and give it to me when done."
+
+game.secret_word = gets.chomp
+puts()
+
+puts "....The host turns his back to Player A, and shows the audience that the secret word is ........"
 puts()
 
 test_count = 3
 while test_count > 0
 
-  puts "Player A, please pick a secret word so we can get started. Write it   down on a piece of paper and give it to me when done."
+  puts "Player A, please guess a word!"
 
   player_guess = gets.chomp
   game.guess_check(player_guess)
   test_count = test_count - 1
+  puts()
+  game.about
 
 end
-game.guess_tracker
+

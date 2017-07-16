f6cad10da78104c2a3886b6d7555604948bd5831
Mitch Mele <mitchmele@gmail.com>
2017-07-16 14:11:21 -0500

    Add method to check and print final outcome of game.

--- ruby/week6solo/word_guess.rb
+++ ruby/week6solo/word_guess.rb
@@ -15,11 +15,12 @@
 #use module mixin for instance methods?
 
 class GuessGame
-  attr_accessor :secret_word
-  attr_reader :guess_check, :guess_tracker, :about
+  attr_accessor :secret_word, :word_display
+  attr_reader :guess_check, :guess_tracker, :about , :game_over
   def initialize
     @guess_log = []
     @guess_count = 3
+    @current_state = []
     @game_over = false
   end
 
@@ -34,22 +35,58 @@ class GuessGame
       @guess_log << @guessed_word
       @guess_count = @guess_count - 1
     end
-    # p @guess_log  # + "#{@guess_count}"?
-    # p "Guesses remaining: #{@guess_count}"
+    @guessed_word
   end
+
+  def guesses
+    @guess_count
+ end
+
+  # def check
+  #   if @guess_log.length == 3
+  #     @guess_log.each do |x|
+  #       if x == "#{@secret_word}"
+  #         return "VICTORY"
+  #       else return "We are done"
+  #     end
+  #   end
+  # end
+  # end
 
   def word_display
     # @current_status = loop through secret word, if letter[i] is in secret word, print letter of i. If not, print "-" !!!!! STOPPING POINT
-     @secret_word
+
+    "#{@guess_log}".split('').each do |x|
+      if @secret_word.include?(x)
+        @current_state << x
+      else
+        @current_state << "-"
+       end
+     end
+    @current_state.uniq.join('')
   end
 
+
+
   def about
-    puts "Here is your current list of word guesses: #{@guess_log}"
-    puts "Guesses remaining: #{@guess_count}"
-    puts word_display
+    puts "Here are your guesses so far: #{@guess_log}"
+    puts()
+    puts "Guesses left: #{@guess_count}"
+    puts()
+    puts "Good guess but not quite.....a few letters(s) from the guess may be in the secret word. If so, here they are:  " + word_display
+    puts()
+
+     @guess_log.each do |x|
+     if x == @secret_word
+       @game_over = true
+      end
+    end
   end
 
 end
+
+
+#TEST CODE
 
 
 # test_game = GuessGame.new  ##This will later be player 1 input?
@@ -67,7 +104,7 @@ puts()
 puts "Player A, please guess what the secret word is. Enter only one guess at a time."
 puts()
 
-puts "Player B, please pick a secret word so we can get started. Write it   down on a piece of paper and give it to me when done."
+puts "Player B, please pick a secret word so we can get started. Write it down on a piece of paper and give it to me when done."
 
 game.secret_word = gets.chomp
 puts()
@@ -75,16 +112,26 @@ puts()
 puts "....The host turns his back to Player A, and shows the audience that the secret word is ........"
 puts()
 
-test_count = 3
-while test_count > 0
+
+while game.guesses > 0
 
   puts "Player A, please guess a word!"
 
   player_guess = gets.chomp
   game.guess_check(player_guess)
-  test_count = test_count - 1
+  # test_count = test_count - 1
   puts()
   game.about
 
+
+  # if game.guesses == 0
+  #   puts "Nice try! Maybe next time when your a contestant on guess your friends secret word game."
+  # end
+
 end
 
+  if game.game_over
+    puts "Congrats! You just one GUESS YOUR FRIENDS SECRET NAME GAME!"
+  else
+    puts "OHHH SO SORRY. IT LOOKS LIKE YOU ARE OUT OF GUESSES. Better luck next time."
+  end

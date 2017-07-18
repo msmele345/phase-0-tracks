0b676824c13112d9b75b4aa8e7e080e502ce4d25
Mitch Mele <mitchmele@gmail.com>
2017-07-18 13:54:11 -0500

    Finish car constructor obj.

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,13 +1,13 @@
-// #Two seperate 4 element arrays
+// // #Two seperate 4 element arrays
 
 
-var horse_Colors = ["brown", "black", "white", "pink"];
+// var horse_Colors = ["brown", "black", "white", "pink"];
 
-var horse_Names = ["Blondie", "Rapunzel", "Spirit", "Northern Dancer"]
+// var horse_Names = ["Blondie", "Rapunzel", "Spirit", "Northern Dancer"]
 
-horse_Colors.push("beige")
+// horse_Colors.push("beige")
 
-horse_Names.push("Always Dreaming")
+// horse_Names.push("Always Dreaming")
 
 
 
@@ -18,14 +18,49 @@ horse_Names.push("Always Dreaming")
 // #Push data into empty object
 // #Return Object
 
-function pair_data(colors, names){
-  var pairobj = {}
-  for(var i = 0; i < colors.length; i++){
-    // console.log(colors[i])
-    // console.log(names[i])
-    pairobj[names[i]] = colors[i]
-  }
+// function pair_data(colors, names){
+//   var pairobj = {}
+//   for(var i = 0; i < colors.length; i++){
+//     // console.log(colors[i])
+//     // console.log(names[i])
+//     pairobj[names[i]] = colors[i]
+//   }
 
-  return pairobj
+//   return pairobj
+// }
+// pair_data(horse_Colors,horse_Names);
+
+
+function Car(make, model, isNew){
+
+  console.log("This car is: ", this);
+
+  this.make = make;
+  this.model = model;
+  this.isNew = isNew;
+
+  this.revEngine = function() { console.log("Froooooommmmmm"); };
+
+  console.log("Car design")
+
 }
-pair_data(horse_Colors,horse_Names);
+
+console.log("Here is another car in the assembly line");
+var newCar = new Car("BMW", "sedan", true);
+console.log(newCar);
+console.log("Our new car is really fast");
+newCar.revEngine();
+
+
+console.log("Here is another car in the assembly line");
+var secondCar = new Car("Chevy", "truck", false)
+console.log(secondCar);
+console.log("This truck is slower, but really loud");
+newCar.revEngine();
+
+
+console.log("Here is another car in the assembly line");
+var thirdCar = new Car ("Mazda", "SUV", true);
+console.log(thirdCar);
+console.log("This is a small suv but it has some horsepower");
+newCar.revEngine();

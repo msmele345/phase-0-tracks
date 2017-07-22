8e2979c30bfa323fcaa8778903a451ea96c0546a
Mitch Mele <mitchmele@gmail.com>
2017-07-22 10:06:25 -0500

    Add conmpareObjects function with tests.

--- js/algos.js
+++ js/algos.js
@@ -20,11 +20,49 @@ function longest_String(string_Array) {
       length = string_Array[i].length
       longest = string_Array[i]
     }
-
   }
-
   console.log(longest);
 }
 
 
+//RELEASE 1
+// Function takes two parameters - both objects
+//Loop through each object and use comparison operators to compare keys from obj1 to the keys of obj2
+//Create default boolean variable to false
+//loop through objects (Two seperate loops?) and return false if there is no match
+//Return true at the end if both loops fail to return false
+//INPUT = Two objects being passed in
+//OUTPUT = True/False boolean if there is a match or not
+
+
+function compareObjects (obj1, obj2){
+  var match = false
+  for(var key1 in obj1){
+    if(obj2[key1] == obj1[key1])
+      match = true;
+  }
+
+  for(var key2 in obj2){
+    if (obj1[key2] == obj2[key2]){
+      match = true;
+    }
+  }
+  console.log(match)
+}
+
+
+
+//Release 1
+compareObjects({name: "Steven", age: 54}, {name: "Tamir", age: 54});
+compareObjects({name: "Steven", age: 54}, {name: "Tamir", age: 27});
+
+
+
+
+//Release 0
 longest_String(["long phrase","longest phrase","longer phrase"]);
+longest_String(["aaaaaaaaaaa", "bbbbbb", "cccccccccccc"])
+
+
+
+

e70bbbe1c413614f11287fd74a6ca6211e5d602b
Mitch Mele <mitchmele@gmail.com>
2017-07-29 10:02:35 -0500

    Setup of all tables. Add data to test.

--- databases/stream_app/stream_helper.rb
+++ databases/stream_app/stream_helper.rb
@@ -8,3 +8,44 @@ require 'faker'
 
 db = SQLite3::Database.new("streaming_data.db")
 
+#Use string delimiters to build tables
+
+customer = <<-SQL
+  CREATE TABLE IF NOT EXISTS streamer (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    password INT,
+    fav_genre VARCHAR(255)
+)
+SQL
+
+matcher = <<-SQL
+  CREATE TABLE IF NOT EXISTS matcher (
+    id INTEGER PRIMARY KEY,
+    match_details VARHCAR(255),
+    watched BOOLEAN,
+    streamer_id INT,
+    selections_id INT,
+    FOREIGN KEY (streamer_id) REFERENCES streamer(id),
+    FOREIGN KEY (selections_id) REFERENCES selections(id)
+  )
+SQL
+
+movies = <<-SQL
+  CREATE TABLE IF NOT EXISTS selections (
+    id INTEGER PRIMARY KEY,
+    title VARCHAR(255),
+    genre VARCHAR(255),
+    stars INT
+  )
+SQL
+
+
+#Create tables
+db.execute(customer)
+db.execute(matcher)
+db.execute(movies)
+
+#TEST with people and movies
+
+

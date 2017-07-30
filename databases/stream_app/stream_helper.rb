b9e870555d91c8993fec3b229aca4d2779edc0b3
Mitch Mele <mitchmele@gmail.com>
2017-07-30 11:03:45 -0500

    Add feedback method and update UI to gather information.

--- databases/stream_app/stream_helper.rb
+++ databases/stream_app/stream_helper.rb
@@ -1,4 +1,4 @@
-##Netflix/Amazon Streaming Choice Selector
+## Streaming devices "Chill" Selector. App helps the user avoid wasting time scrolling through dozens of "suggested titles" that most streaming apps offer and makes a selection for you based of user provided genre preference.
 
 #Require Gems
 require 'sqlite3'
@@ -20,16 +20,15 @@ customer = <<-SQL
 )
 SQL
 
-matcher = <<-SQL
-  CREATE TABLE IF NOT EXISTS matcher (
-    id INTEGER PRIMARY KEY,
-    match_details VARHCAR(255),
-    watched BOOLEAN,
-    streamer_id INT,
-    selections_id INT,
-    FOREIGN KEY (streamer_id) REFERENCES streamer(id),
-    FOREIGN KEY (selections_id) REFERENCES selections(id)
+store_data = <<-SQL
+  CREATE TABLE IF NOT EXISTS data (
+   id INTEGER PRIMARY KEY,
+   name VARCHAR(255),
+   title_match VARCHAR(255),
+   actually_watched BOOLEAN,
+   user_rating INT
   )
+
 SQL
 
 movies = <<-SQL
@@ -44,53 +43,19 @@ SQL
 
 #Create tables
 db.execute(customer)
-db.execute(matcher)
+db.execute(store_data)
 db.execute(movies)
 
 #TEST with people and movies
-
-
 # db.execute("INSERT INTO selections (title, genre, stars) VALUES ('The Rock', 'Action', 4)")
 # db.execute("INSERT INTO selections (title, genre, stars) VALUES ('Trading Places', 'Comedy', 3)")
 
+##USE FAKER TO GENERATE MANY MOVIE names to build selections table
 
-##USE FAKER TO GENERATE MANY MOVIE names
-##Assign each moving name to a genre?
-def create_movie(db, title, genre, stars)
-  db.execute("INSERT INTO selections (title, genre, stars) VALUES (?, ?, ?)", [title,genre, stars])
-end
-
-def user_data(db, name, password, fav_genre)
-  db.execute("INSERT INTO streamer (name, password, fav_genre) VALUES (?, ?, ?)", [name, password, fav_genre])
-end
-
-def combine_data
-  #Include user_data and db as parameters
-  #data = db.execute("INSERT JOIN COMMAND HERE")
-  #data.each?
-  # print combined data
-end
-
-#Pass in user preferences for category into pick movie method
-#assign a search all to variable
-#iterate over selections table
-  #If the user_pref equals the genre key , push the movie title into matches array
-#return random element in the array.
-def pick_movie(db,user_pref)
-  matches = []
-  search = db.execute("SELECT * FROM selections")
-    search.each do |movie|
-      if "#{movie['genre']}" == user_pref
-        matches << "#{movie['title']}"
-      end
-    end
-   puts "You are now watching...#{matches.sample}"
-end
-
-#BUILD TABLES
 #Use combination of UM songs and movie quotes to generate movie names
 #Attach randomly generated genre to each name
-#Attach randomly generated star rating
+#Attach randomly generated star rating to each title
+
 #BUILD MOVIE DATABASE
 # 60.times do
 #   genres = ["Action","Comedy", "Drama", "Sci_fi", "Documentary"]
@@ -102,48 +67,95 @@ end
 #   create_movie(db, Faker::UmphreysMcgee.song , genres[rand(0..4)] , rand(1..5))
 # end
 
+def create_movie(db, title, genre, stars)
+  db.execute("INSERT INTO selections (title, genre, stars) VALUES (?, ?, ?)", [title,genre, stars])
+end
 
+def user_data(db, name, password, fav_genre)
+  db.execute("INSERT INTO streamer (name, password, fav_genre) VALUES (?, ?, ?)", [name, password, fav_genre])
+end
+
+def user_feedback(db, name, title_match, actually_watched, user_rating )
+  #Include user_data and db as parameters
+  #data = db.execute("INSERT JOIN COMMAND HERE")
+  #data.each?
+  db.execute("INSERT INTO data (name, title_match, actually_watched, user_rating) VALUES (?, ?, ?, ?)", [name, title_match, actually_watched, user_rating])
+end
+
+#Pass in user preferences for category into pick movie method
+#assign a search all to variable
+#iterate over selections table
+  #If the user_pref equals the genre key , push the movie title into matches array
+#return random element in the array. This is the key feature! No wasting time scrolling through endless suggestions
+def pick_movie(db,user_pref)
+  matches = []
+  search = db.execute("SELECT * FROM selections")
+    search.each do |movie|
+      if "#{movie['genre']}" == user_pref
+        matches << "#{movie['title']}"
+      end
+    end
+   pick = matches.sample
+   puts "You are now watching...#{pick}"
+   pick
+end
 
 ##Next steps
-# 1. Continue to build UI with tests. Account for bad input with if/else select random if bad input for category?
-    # -Build match method that iterates over data and returns the users request(specific move to avoid add)
-    # -Find way to display relevent match data. Use Join db tables for this. Research.
-    #-Star search?
+#1. Test UI to ensure FALSE trigger on feedback works ok
+#2. Re-try join display in feedback method?
+#3. Add more pseduo as needed to explain intentions
+#4. Build SPECS?
+
 
 
 
 #ORM retrieving data
 
+#Test
 # search = db.execute("SELECT * FROM selections WHERE stars=4")
 # p search
-# search.each do |movie|
-#   if "#{movie['genre']}" == "Sci_fi"
-#     puts "#{movie['title']}"
-#   end
-# end
-
 
 
 ###USER INTERFACE
 
-  user_info = []
+#Call user_data method to insert user data demographic information in db and identify which genre of a movie they want to watch tonight
 
-  puts "Welcome to the Chill starter"
-  puts()
-  puts "What is your name?"
+#Call pick_movie method to iterate over selections table and find a list of movies that meet the user criteria for a genre. Instead of giving the user a long list of movies to have to scroll through and decide, the method returns a random selection and starts the movie right away.
+
+#Ask the user for feedback and a rating of the movie. This data could be analyzed in the future to manage titles based on popularity, provide a viewer review comparison w/the critic review (in the selections table), and help the app determine if the auto-selected movies are even being watched after being started (did the user like or not like that automatic pick). This could help in managing selections inventory.
+
+user_info = []
+
+puts "Welcome to the Chill starter"
+puts()
+puts "What is your name?"
   user_name = gets.chomp
   user_info << user_name
-  puts()
-  puts "What is your password?"
+puts()
+puts "What is your password?"
   pwd = gets.chomp.to_i
   user_info << pwd
-  puts()
-  puts "What are you in the mood for tonight?"
+puts()
+puts "What type of movie are you in the mood for tonight?"
   category = gets.chomp
   user_info << category
 
-p user_info
+# p user_info
 
 user_data(db, user_name, pwd, category)
-pick_movie(db,category)
+app_selection = pick_movie(db,category)
 
+#Ask for feedback from the user. If feedback is provided, call the user_feedback method to store information in data table.
+puts "Would you mind taking a super short survey?"
+  survey = gets.chomp
+  if survey == "Yes"
+      puts "Thank you."
+      puts "Did you actually watch the movie we suggested (True, False)"
+          watched_movie = gets.chomp
+      puts "How would you rate the movie on a scale of 1-5, with 5 being the highest rating possible?"
+          user_rating = gets.chomp.to_i
+          user_feedback(db, user_name, app_selection, watched_movie, user_rating)
+          puts "We appreciate the feedback! Hope to see you in the Chill again soon!"
+  else
+      puts "Thank you for using the Chill Starter! Happy streaming!"
+end

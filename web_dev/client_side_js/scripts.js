a475670ec467cd1184948b3b1fdf1c4a11f10f3a
Mitch Mele <mitchmele@gmail.com>
2017-08-01 15:57:56 -0500

    Add button click html event.

--- web_dev/client_side_js/scripts.js
+++ web_dev/client_side_js/scripts.js
@@ -1,1 +1,27 @@
 console.log("The script is working!")
+
+// var photo = document.getElementById("image");
+// photo.style.border = "2px solid red";
+
+function addBorder (event) {
+  console.log("This worked")
+  console.log(event)
+  event.target.style.border = "14px solid red";
+
+}
+
+var photo = document.getElementById("image");
+photo.addEventListener("click", addBorder);
+
+
+function buttonTest (event){
+  console.log("Button is changing")
+  console.log(event)
+  var text = document.getElementById("text2");
+  text.value = "!!!!!WHOA!!!!"
+}
+
+// Added Input element of text on lines 28-30 to test buttonTest function
+
+var button = document.getElementById("button1");
+button.addEventListener("click", buttonTest)

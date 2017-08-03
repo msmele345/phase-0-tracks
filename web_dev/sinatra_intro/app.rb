ee88b59d1456588182e649850f2b895ea4b132a9
Mitch Mele <mitchmele@gmail.com>
2017-08-03 14:38:08 -0500

    Add get routes for Release 0.

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -8,12 +8,55 @@ db.results_as_hash = true
 # write a basic GET route
 # add a query parameter
 # GET /
+##Release 0 QUESTION 1
+#http://localhost:9393/contact
+get '/contact' do
+  "111 East Mulberry Lane Dallas, TX 54132"
+end
+
+## RELEASE 0  QUESTION 2
+#http://localhost:9393/great_job?name=Mitch
+#Query Parameter example
+get '/great_job' do
+  name = params[:name]
+  if name
+    "Good job, #{name}!"
+  else
+    #http://localhost:9393/great_job
+    "Good Job!"
+  end
+end
+
+## RELEASE 0 QUESTION 3
+#http://localhost:9393/add/6/3
+get '/add/:num1/:num2' do
+  num1 = params[:num1]
+  num2 = params[:num2]
+  result = num1.to_i + num2.to_i
+  "#{num1} is going to be added to #{num2}. The sum of these two numbers is #{result}!"
+end
+
+# http://localhost:9393/?name=Mitch&age=31
 get '/' do
   "#{params[:name]} is #{params[:age]} years old."
 end
 
+# OPTIONAL BONUS ROUTES
+
+get '/ages/:age' do
+  age_check = db.execute("SELECT * FROM students WHERE age=?", [params[:age]])
+  age_check.to_s
+end
+
+get "/campus/:campus" do
+  campus_list = db.execute("SELECT * FROM students WHERE campus=?", [params[:campus]])
+  campus_list.to_s
+end
+
+
 # write a GET route with
 # route parameters
+#ROUTE PARAMETER EX
 get '/about/:person' do
   person = params[:person]
   "#{person} is a programmer, and #{person} is learning Sinatra."
@@ -23,7 +66,6 @@ get '/:person_1/loves/:person_2' do
   "#{params[:person_1]} loves #{params[:person_2]}"
 end
 
-# write a GET route that retrieves
 # all student data
 get '/students' do
   students = db.execute("SELECT * FROM students")
